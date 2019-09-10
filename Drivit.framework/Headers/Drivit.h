9c2434ebb9cde3247196e7cce717b7f2b69c051f
Adão Rodrigues <adao.rodrigues@drivit.com>
2019-09-10 14:46:25 +0100

    Upload 2.0.3 version

---
+++ Drivit.framework/Headers/Drivit.h
@@ -0,0 +1,12 @@
+//
+//  Drivit.h
+//  Drivit
+//
+//  Created by Gonçalo Rodrigues Braz Frade on 04/05/18.
+//  Copyright © 2018 Drivit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double DrivitVersionNumber;
+FOUNDATION_EXPORT const unsigned char DrivitVersionString[];
